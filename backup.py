ccaf8e344ce19357867c195ae9f0647037d47bc4
unknown <marc@.(none)>
2011-09-02 17:30:32 +0700

    add looping through config

--- backup.py
+++ backup.py
@@ -1,6 +1,16 @@
 import json
+import sys
+import os
+import subprocess
 
 if __name__ == "__main__":
-    print "what up"
-    data = json.load(open('backup.json'))
-    print data
+    config_location = sys.argv[1]
+    
+    config = json.load(open(config_location))
+    for db_name, settings in config.items():
+        print db_name
+        print settings
+        
+        os.chdir(settings['dir'])
+        
+        retcode = subprocess.call(["mysqldump", "-u"+settings['user'], "-p"+settings['pass'], "-r foo_db.dump"])
